27e877db9c59df31cc391c53de018f27a1027041
Mark Saruni <narksaruni23@gmail.com>
2024-11-09 18:03:54 +0300

    Add STATIC_ROOT to settings.py

--- tech_api/tech_api/settings.py
+++ tech_api/tech_api/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 
 # Static and media files
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
