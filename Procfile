17b61ffbe1aa925a4d5580f60c63b950dffdecc2
Mark23Dev <sarunimark06@gmail.com>
2024-11-12 19:21:01 +0300

    Deleted configuration for documentation

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: gunicorn tech_api.wsgi:application --log-file -
-release: python tech_api/manage.py migrates
--- tech_api/tech_api/settings.py
+++ tech_api/tech_api/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 
 # Static and media files
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
